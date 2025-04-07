8fde7b46d8ca7a2df1dc47bda2bddc8fe1a138b0
Yaniel Fundora Curbelo <yfundorac92@gmail.com>
2025-04-07 22:04:06 +0100

    add document and odfloadr

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
+import { ChatOpenAI } from "@langchain/openai";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 // import { OpenAIEmbeddings } from "@langchain/openai";
 // import { Chroma } from "@langchain/community/vectorstores/chroma";
-import { ChatOpenAI } from "@langchain/openai";
-import { HumanMessage, SystemMessage } from "@langchain/core/messages";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
+// import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+// import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { Document } from "@langchain/core/documents";
 
 const model = new ChatOpenAI({ model: "gpt-4o-mini" });
 // Load environment variables from .env file
@@ -11,42 +13,43 @@ dotenv.config();
 
 // Access environment variables
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-console.log('OPENAI_API_KEY:', OPENAI_API_KEY);
 
-const systemTemplate = "Translate the following from English into {language}";
+// const systemTemplate = "Translate the following from English into {language}";
 
-const promptTemplate = ChatPromptTemplate.fromMessages([
-    ["system", systemTemplate],
-    ["user", "{text}"],
-  ]);
+// const promptTemplate = ChatPromptTemplate.fromMessages([
+//     ["system", systemTemplate],
+//     ["user", "{text}"],
+//   ]);
 
-  const prompt = await promptTemplate.invoke({
-    language: "Spanish",
-    text: "Hi there"
-  })
+//   const prompt = await promptTemplate.invoke({
+//     language: "Spanish",
+//     text: "Hi there"
+//   })
 
-const result = await model.invoke(prompt);
-console.log(result.content);
-
-/* 
-const stream = await model.stream(messages);
-
-const chunks = [];
-for await (const chunk of stream) {
-  chunks.push(chunk);
-  console.log(`${chunk.content}|`);
-}
+// const result = await model.invoke(prompt);
+// console.log(result.content);
 
 
-const embeddings = new OpenAIEmbeddings({
-    model: "text-embedding-3-large"
-  });
+const documents = [
+  new Document({
+    pageContent:
+      "Dogs are great companions, known for their loyalty and friendliness.",
+    metadata: { source: "mammal-pets-doc" },
+  }),
+  new Document({
+    pageContent: "Cats are independent pets that often enjoy their own space.",
+    metadata: { source: "mammal-pets-doc" },
+  }),
+];
 
-  const vectorStore = new Chroma(embeddings, {
-    collectionName: "a-test-collection",
-  });
-  
-  console.log(vectorStore);*/
+console.log(documents);
+
+const loader = new PDFLoader("./assets/insurance.pdf");
+
+const docs = await loader.load();
+console.log(docs[0].pageContent);
+console.log(docs[0].metadata);
+
 
 
 
