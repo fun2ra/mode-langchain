b85b7bb04d9cddb5b168fabc0bfaae01e5c532c8
Yaniel Fundora Curbelo <yfundorac92@gmail.com>
2025-04-08 15:20:19 +0100

    add emeddings

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import { ChatOpenAI } from "@langchain/openai";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-// import { OpenAIEmbeddings } from "@langchain/openai";
-// import { Chroma } from "@langchain/community/vectorstores/chroma";
+import { Document } from "@langchain/core/documents";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+
 // import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 // import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { Document } from "@langchain/core/documents";
 
 const model = new ChatOpenAI({ model: "gpt-4o-mini" });
 // Load environment variables from .env file
 dotenv.config();
-
-// Access environment variables
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 // const systemTemplate = "Translate the following from English into {language}";
 
@@ -29,7 +28,6 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 // const result = await model.invoke(prompt);
 // console.log(result.content);
 
-
 const documents = [
   new Document({
     pageContent:
@@ -42,15 +40,37 @@ const documents = [
   }),
 ];
 
-console.log(documents);
-
 const loader = new PDFLoader("./assets/insurance.pdf");
 
 const docs = await loader.load();
-console.log(docs[0].pageContent);
-console.log(docs[0].metadata);
 
+const textSplitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 1000,
+  chunkOverlap: 200,
+});
 
+const allSplits = await textSplitter.splitDocuments(docs);
 
+console.log(allSplits.length);
+
+const embeddings = new OpenAIEmbeddings({
+  model: "text-embedding-3-large",
+});
+
+// const vector1 = await embeddings.embedQuery(allSplits[0].pageContent);
+// const vector2 = await embeddings.embedQuery(allSplits[1].pageContent);
+
+const vectorStore = await MemoryVectorStore.fromDocuments(
+  allSplits,
+  embeddings
+);
+
+const results1 = await vectorStore.similaritySearch(
+  "What are the classifications of insurance?",
+);
+
+console.log(results1);
+
+// Documents are already added via fromDocuments()
 
 // Your application logic here
